c70c748120dedc1111b8d4054baff170a33df3e9
brfabri <bruno.fabri19@gmail.com>
2017-06-23 23:31:22 -0300

    Add files via upload

--- 01.idade_anos_meses_dias.py
+++ 01.idade_anos_meses_dias.py
@@ -11,12 +11,22 @@ anoan=int(input("Insira o ano de seu nascimento: "))
 
 idade=anoat-anoan
 
-if  mesan<mesat:
-    qtdmes=mesat-mesan
-    print("Você têm ",idade," anos, ",qtdmes," meses de vida")
+if  mesat>mesan:
+    mes=mesat-mesan
+    if diaan>diaat:
+        qtddia=(30-diaan)+diaat
+        qtdmes=mes-1
+        print("Você têm ",idade," anos, ",qtdmes," meses e",qtddia," dias de vida")
+    else:
+        qtddia=diaat-diaan
+        print("Você têm ",idade," anos, ",mes," meses e",qtddia," dias de vida")
 elif  mesan>mesat:
-    print(idade-1)
-elif diaan==diaat or diaan<diaat and mesan==mesat:
-    print(idade)
-else:
-    print(idade-1)
+    qtdanos=idade-1
+    qtdmes=(12-mesan)+mesat
+    if diaan>diaat:
+        qtddia=(30-diaan)+diaat
+        mes=qtdmes-1
+        print("Você têm ",qtdanos," anos, ",qtdmes," meses e",qtddia," dias de vida")
+    else:
+        qtddia=diaat-diaan
+        print("Você têm ",qtdanos," anos, ",qtdmes," meses e",qtddia," dias de vida")
